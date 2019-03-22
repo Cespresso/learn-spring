0cd5373a7a7148dad99b22d690de8aa4206f2960
Cespresso <2cespresso@gmail.com>
2019-03-22 21:49:24 +0900

    モデルの項目の変更

--- src/main/kotlin/cespresso/gmail/com/todo/domain/entity/Todos.kt
+++ src/main/kotlin/cespresso/gmail/com/todo/domain/entity/Todos.kt
@@ -8,12 +8,16 @@ class Todos (
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="id")
-    var id: Long = 0,
+    var id: Long? = null,
+    @Column(name="user_id")
+    var user_id: String? = null,
     @Column(name="title")
-    var title: String = "",
-    @Column(name="description")
-    val description:String ="",
+    var title: String? = null,
+    @Column(name="completed")
+    val completed:Boolean? =null,
     @Column(name="body")
-    var body: String = ""
+    var body: String? = null
 
-)
+){
+    constructor(_title: String?,_completed: Boolean?,_body: String?):this(title = _title,completed = _completed,body = _body)
+}
